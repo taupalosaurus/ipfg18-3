b90d5b7f4192b93c4fabec4052a32d4038f940dc
Nicolas Barral <n.barral@imperial.ac.uk>
2018-10-31 17:17:40 +0000

    follow main repo

--- tests/question-3_7.py
+++ tests/question-3_7.py
@@ -6,7 +6,23 @@ test = {
       'cases': [
         {
           'code': r"""
-          >>> np.allclose(myprimes, [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97])
+          >>> type(prime_list(10)) == list
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': "",
+      'teardown': '',
+      'type': 'doctest'
+    },
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> np.allclose(prime_list(20), [2, 3, 5, 7, 11, 13, 17, 19])
           True
           """,
           'hidden': False,
@@ -14,7 +30,7 @@ test = {
         },
         {
           'code': r"""
-          >>> type(myprimes) == list
+          >>> np.allclose(prime_list(100), [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97])
           True
           """,
           'hidden': False,
